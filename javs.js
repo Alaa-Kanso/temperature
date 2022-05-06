30e4048730148053cf2ad480f7c0e97779042804
AlaaKanso <105053234+AlaaKanso@users.noreply.github.com>
2022-05-06 17:43:45 +0300

    Update javs.js

--- javs.js
+++ javs.js
@@ -2,7 +2,7 @@ let celc = document.getElementById('cel');
 let ferh = document.getElementById('fer');
 
 
-//cel to fer when you write the value in cel it will show in fer n other box
+//cel to fer when you write the value in cel it will show in fer 
 celc.oninput = () => {
     let output = (parseFloat(celc.value)*9)/5 +32;
     ferh.value = parseFloat(output.toFixed(2));
