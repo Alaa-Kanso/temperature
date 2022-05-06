ead33715da0d4a609ea894ac777ef3c845638a38
AlaaKanso <alaakanso120@gmail.com>
2022-05-06 17:40:08 +0300

    temperature converter
    a small javascript project to practice my newly acquired skill

---
+++ javs.js
@@ -0,0 +1,14 @@
+let celc = document.getElementById('cel');
+let ferh = document.getElementById('fer');
+
+
+//cel to fer when you write the value in cel it will show in fer n other box
+celc.oninput = () => {
+    let output = (parseFloat(celc.value)*9)/5 +32;
+    ferh.value = parseFloat(output.toFixed(2));
+};
+
+ferh.oninput = () => {
+    let output = ((parseFloat(ferh.value) - 32)*5)/9;
+    celc.value = parseFloat(output.toFixed(2));
+};
